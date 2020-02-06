fda2f64bcc3d3e68466643fcb7bec2d7199bae43
admin <rajnish.kahar1996@gmail.com>
2020-02-06 16:49:34 +0530

    Add : mockito to check positive and negetive senario

--- src/test/java/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -9,7 +9,7 @@ import org.mockito.junit.MockitoRule;
 import static org.mockito.Mockito.when;
 
 public class CabInvoiceGeneratorTest {
-    InvoiceSevice cabInvoiceGenerator = null;
+    InvoiceSevice invoiceSevice = null;
 
     @Mock
     RideRepository rideRepository;
@@ -19,14 +19,15 @@ public class CabInvoiceGeneratorTest {
 
     @Before
     public void setUp() {
-        cabInvoiceGenerator = new InvoiceSevice(new RideRepository());
+
+        invoiceSevice = new InvoiceSevice(rideRepository);
     }
 
     @Test
     public void givenDistanceAndTime_shouldReturnTotalFare() {
         double dist = 2.0;
         int time = 5;
-        double totalFare = cabInvoiceGenerator.getTotalFare(dist, time);
+        double totalFare = invoiceSevice.getTotalFare(dist, time);
         Assert.assertEquals(25.0, totalFare, 0);
     }
 
@@ -34,7 +35,7 @@ public class CabInvoiceGeneratorTest {
     public void givenLessDistanceAndTime_shouldReturnMinFare() {
         double dist = 0.1;
         int time = 1;
-        double totalFare = cabInvoiceGenerator.getTotalFare(dist, time);
+        double totalFare = invoiceSevice.getTotalFare(dist, time);
         Assert.assertEquals(5.0, totalFare, 0);
     }
 
@@ -43,7 +44,7 @@ public class CabInvoiceGeneratorTest {
         Ride[] ride1 = {new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
-        InvoiceSummary summary = cabInvoiceGenerator.calculateFare(ride1);
+        InvoiceSummary summary = invoiceSevice.calculateFare(ride1);
         InvoiceSummary exceptedInvoiceSummary = new InvoiceSummary(2, 30.0, 15.0);
         Assert.assertEquals(exceptedInvoiceSummary, summary);
     }
@@ -54,8 +55,8 @@ public class CabInvoiceGeneratorTest {
         Ride[] ride1 = {new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
-        cabInvoiceGenerator.addRides(userId, ride1);
-        InvoiceSummary summary = cabInvoiceGenerator.getInvoiceSummary(userId);
+        invoiceSevice.addRides(userId, ride1);
+        InvoiceSummary summary = invoiceSevice.getInvoiceSummary(userId);
         System.out.println(summary);
         InvoiceSummary exceptedInvoiceSummary = new InvoiceSummary(2, 30.0, 15.0);
         Assert.assertEquals(exceptedInvoiceSummary, summary);
@@ -73,5 +74,17 @@ public class CabInvoiceGeneratorTest {
         InvoiceSummary exceptedInvoiceSummary = new InvoiceSummary(2, 30.0, 15.0);
         InvoiceSummary summary = invoiceSevice.getInvoiceSummary(userId);
         Assert.assertEquals(exceptedInvoiceSummary, summary);
+    }
+
+    @Test
+    public void givenUserId_whenNotCorrect_shouldReturnExcption() {
+        String userId = null;
+        try {
+            when(rideRepository.getRides(userId)).thenThrow(new RuntimeException("Invalid Input"));
+            invoiceSevice.getInvoiceSummary(userId);
+        } catch (RuntimeException e) {
+            Assert.assertEquals(e.getMessage(), "Invalid Input");
+            e.printStackTrace();
+        }
     }
 }
